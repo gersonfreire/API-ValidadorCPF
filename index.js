61dab0c130bf5a8ef130cd3683a999aeef67e218
gerson <gerson.freire@gmail.com>
2024-10-31 14:48:25 -0300

    Update dotenv configuration to load environment variables from .env file

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const bodyParser = require('body-parser');
 
 // Load environment variables from an alternate .env file
-dotenv.config({ path: './bigvps.env' });
+dotenv.config({ path: '.env' });
 
 // Controllers
 const ControllerCPF = require('./src/controllers/ControllerCPF.js');
